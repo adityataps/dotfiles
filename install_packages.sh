e70b829f922cb59a3fb7366ee215260a311abe2c
Aditya Tapshalkar <aditya.tapshalkar@apptega.com>
2024-10-21 10:52:58 -0400

    brew: add tree, add brew env script

--- install_packages.sh
+++ install_packages.sh
@@ -4,6 +4,7 @@
 if ! command -v brew &> /dev/null; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Homebrew is already installed."
 fi
