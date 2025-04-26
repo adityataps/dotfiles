ae7eb380dcbb2cbc7a8862d77c85720a9e6afd64
Aditya Tapshalkar <aditya.taps@gmail.com>
2025-04-26 00:50:58 -0400

    fix: update exec command

--- postmake.sh
+++ postmake.sh
@@ -6,4 +6,4 @@ git config --global user.name "$DOTFILE_GITCONFIG_USER"
 git config --global user.email "$DOTFILE_GITCONFIG_EMAIL"
 
 # Restart the current shell to use the updated configs
-exec $SHELL
+$SHELL
