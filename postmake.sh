43a06bd388c9971bca35918779c5a18b3d5c1c24
Aditya Tapshalkar <aditya.taps@gmail.com>
2025-04-25 21:31:32 -0400

    feat(gitconfig): add script to iterate over git repos recursively in explicit safe directory

--- postmake.sh
+++ postmake.sh
@@ -4,6 +4,7 @@
 # Git
 git config --global user.name "$DOTFILE_GITCONFIG_USER"
 git config --global user.email "$DOTFILE_GITCONFIG_EMAIL"
+find "$GITCONFIG_SAFE_DIRECTORY" -name '.git' -type d -exec bash -c 'git config --global --add safe.directory ${0%/.git}' {} \;
 
 # Restart the current shell to use the updated configs
 $SHELL
