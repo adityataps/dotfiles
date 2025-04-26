6722e87feb86381ea95511409fbea677dd6d9a0f
Aditya Tapshalkar <aditya.taps@gmail.com>
2025-04-26 01:39:29 -0400

    fix: move CUDA path update out of zshrc

--- postmake.sh
+++ postmake.sh
@@ -8,5 +8,9 @@ git config --global user.email "$DOTFILE_GITCONFIG_EMAIL"
 
 [[ -v GITCONFIG_SAFE_DIRECTORY ]] && find "$GITCONFIG_SAFE_DIRECTORY" -name '.git' -type d -exec bash -c 'git config --global --add safe.directory ${0%/.git}' {} \; || echo "GITCONFIG_SAFE_DIRECTORY is not set";
 
+# CUDA
+export PATH=/usr/local/cuda-12.8/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-12.8/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
 # Restart the current shell to use the updated configs
 $SHELL
