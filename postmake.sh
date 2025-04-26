d63f65754eb8f99736080d2e725bcac678bc35d6
Aditya Tapshalkar <aditya.taps@gmail.com>
2025-04-26 01:08:07 -0400

    fix: set safe directory only if variable is set

--- postmake.sh
+++ postmake.sh
@@ -5,7 +5,8 @@
 git config --global user.name "$DOTFILE_GITCONFIG_USER"
 git config --global user.email "$DOTFILE_GITCONFIG_EMAIL"
 [ "$(uname -s)" = "Darwin" ] && git config --global credential.helper osxkeychain  
-find "$GITCONFIG_SAFE_DIRECTORY" -name '.git' -type d -exec bash -c 'git config --global --add safe.directory ${0%/.git}' {} \;
+
+[[ -v GITCONFIG_SAFE_DIRECTORY ]] && find "$GITCONFIG_SAFE_DIRECTORY" -name '.git' -type d -exec bash -c 'git config --global --add safe.directory ${0%/.git}' {} \; || echo "GITCONFIG_SAFE_DIRECTORY is not set";
 
 # Restart the current shell to use the updated configs
 $SHELL
