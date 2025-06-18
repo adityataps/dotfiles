742d577d22c3690015ffc81d71eea09c46a2bf63
Aditya Tapshalkar <aditya.taps@gmail.com>
2025-06-18 11:48:16 -0400

    feat: add credential helper if macos

--- postmake.sh
+++ postmake.sh
@@ -4,6 +4,7 @@
 # Git
 git config --global user.name "$DOTFILE_GITCONFIG_USER"
 git config --global user.email "$DOTFILE_GITCONFIG_EMAIL"
+[ "$(uname -s)" = "Darwin" ] && git config --global credential.helper osxkeychain
 
 # Restart the current shell to use the updated configs
 $SHELL
